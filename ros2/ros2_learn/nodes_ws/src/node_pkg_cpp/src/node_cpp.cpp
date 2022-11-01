0a4c417cf5f8ae97220e3c340ad1a6ead6186ff0
calvin d. woo <calvin.d.woo@gmail.com>
2022-11-01 21:56:12 +0900

    start learning ros2 nodes

---
+++ ros2/ros2_learn/nodes_ws/src/node_pkg_cpp/src/node_cpp.cpp
@@ -0,0 +1,39 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+
+using namespace std::chrono_literals;
+
+class MyROSNode : public rclcpp::Node
+{
+public:
+  MyROSNode()
+      : Node("intro_ros2_node_cpp"), count_(0)
+  {
+    timer_ = this->create_wall_timer(
+        1000ms,
+        std::bind(&MyROSNode::timer_callback, this)
+    );
+  }
+
+private:
+  void timer_callback()
+  {
+    std::string message = "Hello from the Hadabot ROS 2 intro C++ node: " + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "'%s'", message.c_str());
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  size_t count_;
+};
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  auto my_ros_node = std::make_shared<MyROSNode>();
+  rclcpp::spin(my_ros_node);
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ ros2/ros2_learn/nodes_ws/src/node_pkg_py/node_pkg_py/node_py.py
@@ -0,0 +1,35 @@
+import rclpy
+from rclpy.node import Node
+
+
+class MyROSNode(Node):
+
+    def __init__(self):
+        super().__init__('intro_ros2_node_py')
+
+        timer_period_in_sec = 1.0
+        self.timer = self.create_timer(
+            timer_period_in_sec,
+            self.timer_callback
+        )
+        self.i = 0
+
+    def timer_callback(self):
+        self.get_logger().info(
+            'Hello from the Hadabot ROS 2 intro Python node: "%d"' % self.i)
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    my_ros_node = MyROSNode()
+
+    rclpy.spin(my_ros_node)
+
+    my_ros_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
