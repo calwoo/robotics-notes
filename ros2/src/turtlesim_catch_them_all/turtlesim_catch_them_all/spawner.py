6bcce362335498fbb22ea3b841414ac35638e0fc
calvin d. woo <calvin.d.woo@gmail.com>
2023-07-03 14:18:11 -0400

    start turtlesim catcher project

---
+++ ros2/src/turtlesim_catch_them_all/turtlesim_catch_them_all/spawner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+class TurtleSpawner(Node):
+    def __init__(self):
+        super().__init__("turtle_spawner")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TurtleSpawner()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
