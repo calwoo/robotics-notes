28d23d86a00c6a29b04f51fffc6436a852f7490d
calvin d. woo <calvin.d.woo@gmail.com>
2023-07-03 18:43:01 -0400

    finish spawner node

--- ros2/src/turtlesim_catch_them_all/turtlesim_catch_them_all/spawner.py
+++ ros2/src/turtlesim_catch_them_all/turtlesim_catch_them_all/spawner.py
@@ -6,22 +6,31 @@ from functools import partial
 import rclpy
 from rclpy.node import Node
 from turtlesim.srv import Spawn, Kill
+from turtlesim_spawn_interfaces.msg import Turtle, TurtleArray
+from turtlesim_spawn_interfaces.srv import CatchTurtle
 
 
 class TurtleSpawner(Node):
     def __init__(self):
         super().__init__("turtle_spawner")
+
+        # spawn service
         self.spawn_client = self.create_client(Spawn, "spawn")
         while not self.spawn_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("service not available, waiting again...")
 
-        # self.kill_service = ?
-
-        spawn_period = 2.0
+        spawn_period = 5.0
         self.spawn_timer = self.create_timer(spawn_period, self.spawn_turtle)
 
-        # turtle store
+        # kill service
+        self.kill_service = self.create_service(CatchTurtle, "kill", self.kill_turtle)
+
+        # turtle status topic pub
         self.turtles = []
+        self.alive_publisher = self.create_publisher(TurtleArray, "alive_turtles", 10)
+
+        pub_timer_period = 0.5
+        self.pub_timer = self.create_timer(pub_timer_period, self.broadcast_turtles)
 
     def spawn_turtle(self):
         self.get_logger().info("spawning turtle!")
@@ -39,20 +48,43 @@ class TurtleSpawner(Node):
 
     def callback_spawn_turtle(self, future, turtle_x, turtle_y):
         try:
-            turtle_name = future.result()
-            self.get_logger().info(f"spawned turtle {turtle_name} @ x={turtle_x} y={turtle_y}")
+            response = future.result()
+            self.get_logger().info(f"spawned turtle {response.name} @ x={turtle_x} y={turtle_y}")
         except Exception as e:
             self.get_logger().error(f"service call failed: {e}")
 
         turtle = {
             "x": turtle_x,
             "y": turtle_y,
-            "name": turtle_name
+            "name": response.name
         }
         self.turtles.append(turtle)
 
-    def kill_turtle(self):
-        pass
+    def kill_turtle(self, request, response):
+        turtle_name = request.turtle_name
+        for turtle in self.turtles:
+            if turtle["name"] == turtle_name:
+                # remove turtle
+                self.turtles.remove(turtle)
+                response.return_code = 1
+                return response
+        
+        # no turtle found with this name
+        response.return_code = 0
+        return response
+    
+    def broadcast_turtles(self):
+        array_msg = TurtleArray()
+        array_msg.turtles = []
+        for turtle in self.turtles:
+            turtle_msg = Turtle(
+                x=turtle["x"],
+                y=turtle["y"],
+                name=turtle["name"]
+            )
+            array_msg.turtles.append(turtle_msg)
+
+        self.alive_publisher.publish(array_msg)
 
 def main(args=None):
     rclpy.init(args=args)
